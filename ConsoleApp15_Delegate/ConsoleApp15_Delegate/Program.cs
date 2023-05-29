45d1326fb6cc88b7cc2a320be255ff402b36917c
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-29 14:00:49 +0530

    Add files via upload

---
+++ ConsoleApp15_Delegate/ConsoleApp15_Delegate/Program.cs
@@ -0,0 +1,62 @@
+﻿
+delegate string ChildDelegate();
+
+delegate void Mystudy(string n);
+
+delegate int AgeDelegate();
+class Boys
+{
+        public string Name()
+    {
+        return "Ankit";
+    }
+
+    public string Address()
+    {
+        return "Noida";
+    }
+
+    public int Age()
+    {
+        return 25;
+        
+    }
+
+    public void study(string pn)
+    {
+        Console.WriteLine(pn);
+    }
+
+
+
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Boys obj = new Boys();
+
+        ChildDelegate play = new ChildDelegate(obj.Name);
+        
+
+        var temp = play();
+        Console.WriteLine("Name :" + temp);
+
+        play += obj.Address;
+
+        var temp1 = play();
+        Console.WriteLine("Address :" + temp1);
+
+        Mystudy objp=new Mystudy(obj.study);
+        objp("Education :" + "Beacholer");
+
+        AgeDelegate obj1 = new AgeDelegate(obj.Age);
+        var temp2 = obj1();
+        Console.WriteLine("Age is :" + temp2);
+
+
+
+
+    }
+}
